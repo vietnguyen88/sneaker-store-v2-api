959515562831d3a5457ad81a6117296e08e8faee
Viet Nguyen <vietnguyen@Viets-MacBook-Pro.local>
2020-06-21 11:43:51 +1000

    add upload feature

--- routes/products.js
+++ routes/products.js
@@ -1,62 +1,74 @@
-const router = require('express').Router();
-let Product = require('../models/product.model')
+const router = require("express").Router();
+let Product = require("../models/product.model");
 
+router.route("/").get((req, res) => {
+  Product.find()
+    .then((products) => res.json(products))
+    .catch((err) => console.log(err));
+});
 
-router.route('/').get((req, res) => {
-    Product.find()
-        .then(products => res.json(products))
-        .catch(err => console.log(err))
-}
-)
+router.route("/create").post((req, res) => {
+  const product = {
+    productName: req.body.productName,
+    productDesc: req.body.productDesc,
+    productPrice: req.body.productPrice,
+    productUrl: req.body.productUrl,
+    productCategory: req.body.productCategory,
+  };
+  const newProduct = new Product(product);
 
-router.route('/create').post((req, res) => {
-    const product = {
-        productName: req.body.productName,
-        productDesc: req.body.productDesc,
-        productPrice: req.body.productPrice,
-        productUrl: req.body.productUrl,
-        productCategory: req.body.productCategory
-    }
-    const newProduct = new Product(product)
+  newProduct
+    .save()
+    .then(() => res.json("Product created"))
+    .catch((err) => res.status(400).json(err));
+});
 
-    newProduct.save()
-        .then(() => res.json('Product created'))
-        .catch(err => res.status(400).json(err))
-})
+router.route("/:id").get((req, res) => {
+  const id = req.params.id;
 
-router.route('/:id').get((req, res) => {
-    const id = req.params.id
+  Product.findById(id)
+    .then((product) => res.json(product))
+    .catch((err) => res.status(400).json(err));
+});
 
-    Product.findById(id)
-        .then(product => res.json(product))
-        .catch(err => res.status(400).json(err))
-})
+router.route("/update/:id").post((req, res) => {
+  const id = req.params.id;
 
-router.route('/update/:id').post((req, res) => {
-    const id = req.params.id
+  const product = {
+    productName: req.body.productName,
+    productDesc: req.body.productDesc,
+    productPrice: req.body.productPrice,
+    productUrl: req.body.productUrl,
+    productCategory: req.body.productCategory,
+  };
 
-    const product = {
-        productName: req.body.productName,
-        productDesc: req.body.productDesc,
-        productPrice: req.body.productPrice,
-        productUrl: req.body.productUrl,
-        productCategory: req.body.productCategory
-    }
+  Product.findByIdAndUpdate(id, product)
+    .then(() => res.json("Product updated"))
+    .catch((err) => res.status(400).json(err));
+});
 
-    Product.findByIdAndUpdate(id, product)
-        .then(() => res.json('Product updated'))
-        .catch(err => res.status(400).json(err))
-})
+router.route("/delete/:id").delete((req, res) => {
+  const id = req.params.id;
 
+  Product.findByIdAndDelete(id)
+    .then(() => res.json("product deleted"))
+    .catch((err) => res.status(400).json(err));
+});
 
-router.route('/delete/:id').delete((req, res) => {
-    const id = req.params.id
+router.route("/upload").post((req, res) => {
+    console.log(__dirname);
+  if (req.files === null) {
+    return res.status(400).json("no file");
+  }
+  const file = req.files.file;
+  file.mv(`${__dirname}/sneaker-store-v2/public/uploads/${file.name}`, (err) => {
+   if(err){
+       return res.status(500).send(err)
+   }
+    res.json(file)
+  }
+  
+  );  
+});
 
-    Product.findByIdAndDelete(id)
-        .then(() => res.json('product deleted'))
-        .catch(err => res.status(400).json(err))
-
-})
-
-
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const env = require('dotenv')
+const fileUpload = require('express-fileupload')
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const app = express();
 app.use(cors());
 app.use(express.json())
+app.use(fileUpload)
 env.config();
 
 const port = process.env.PORT || 4000;
@@ -25,6 +27,7 @@ const ProductsRouter = require('./routes/products')
 
 app.use('/users', usersRouter);
 app.use('/products', ProductsRouter)
+
 
 
 
